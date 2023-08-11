b94571ed0fc2f01e993a1b4ba63e837d229680c8
DianeBada <92454613+DianeBada@users.noreply.github.com>
2023-08-11 13:37:33 +0200

    changing things

--- former.php
+++ former.php
@@ -1,3 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
 <?php
 //get data from form  
 
@@ -8,7 +17,7 @@ $note = $_POST['note'];
 $mobile = $_POST['mobile'];
 $to = "ogediane20@gmail.com";
 $subject = "New Green Line Energy Enquiry";
-$txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message . "\r\n Mobile number =". $mobile . "\r\n Note =". $note;
+$txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Mobile number =". $mobile . "\r\n Note =". $note;
 $headers = "From: noreply@greenlineenergy.com" . "\r\n" .
 "CC: somebodyelse@example.com";
 if($email!=NULL){
@@ -17,3 +26,5 @@ if($email!=NULL){
 //redirect
 header("Location:404.html");
 ?>
+</body>
+</html>
